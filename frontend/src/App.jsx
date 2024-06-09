194255e274a8a7303b497065eeb8b0a353013e75
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-06-09 21:23:57 +0300

    design createTodo using bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './App.css'
 
 import Todos from './components/Todos'
@@ -8,12 +10,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <CreateToDo />
-      <Todos />
+    <div className='container'>
+      <div className='row'>
+        <div className='col-lg-7 mx-auto my-5'>
+        <CreateToDo />
+        <Todos />
 
+
+        </div>
+      </div>
     </div>
   )
 }
 
 export default App
+
--- frontend/src/components/CreateToDo.jsx
+++ frontend/src/components/CreateToDo.jsx
@@ -7,7 +7,24 @@ const CreateToDo = () => {
 
   // UI + Logic
   return(
-    <h3> Create ToDo</h3>
+    <div className="container mt-4">
+      <form className="row w-100 align-items-center">
+        <div className="col-8">
+          <input type="text" className="form-control" placeholder="Enter Your ToDo"></input>
+          </div>
+          <div className="col-2">
+            <select className="form-control">
+              <option value="DONE"> Done </option>
+              <option value="INPROGRESS"> InProgress </option>
+            </select>
+          </div>
+          <div className="col-2">
+            <input type="submit" value="Add" className="btn btn-primary" />
+
+          </div>
+      </form>
+
+    </div>
   )
 }
 
