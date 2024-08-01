adc8092ba20fa49ae888e06a2f5e04ee2660e534
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-08-01 13:52:03 +0300

    add dark mode

--- jobboard/components/Navbar.tsx
+++ jobboard/components/Navbar.tsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button"
 import { Shell } from 'lucide-react';
+import { ModeToggle } from "@/components/ToggleButton"
 
 
 function NavBar() {
   return (
-    <div className="flex items-center justify-between px-5 py-4 bg-transparent">
+    <div className="flex items-center justify-between py-3 px-5 bg-transparent border-b-2">
       <Link href='/' className="flex items-center gap-2">
       <Shell className="w-8 h-8"/>
         <span className="text-lg">JobBoard</span>
       </Link>
-      <div>
-        <Button asChild>
+      <div className="flex ">
+        <Button asChild className="mr-2">
           <Link href="/login">Login</Link>
         </Button>
+        <ModeToggle />
 
       </div>
     </div>
