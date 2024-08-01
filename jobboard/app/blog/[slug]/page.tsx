a69d49a6b7c8ac8f47111efe7d2a03b40f74cbcf
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-08-01 13:58:11 +0300

    add blog folders

---
+++ jobboard/app/blog/[slug]/page.tsx
@@ -0,0 +1,12 @@
+
+function PostDetail(){
+
+    return(
+        <div>
+            <h3> Post Detail </h3>
+        </div>
+    )
+
+}
+
+export default PostDetail
---
+++ jobboard/app/blog/page.tsx
@@ -0,0 +1,13 @@
+
+
+function PostList(){
+
+    return(
+        <div>
+            <h3> Post List </h3>
+        </div>
+    )
+
+}
+
+export default PostList
