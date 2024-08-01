65484bfd9bb8eefe13e60fcb1e267b372b8d2222
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-08-01 14:55:06 +0300

    add blog list & detail new pages

--- jobboard/app/blog/[slug]/page.tsx
+++ jobboard/app/blog/[slug]/page.tsx
@@ -2,8 +2,12 @@
 function PostDetail(){
 
     return(
-        <div>
-            <h3> Post Detail </h3>
+        <div className="container mx-auto p-6">
+            <div className="max-w-xl mx-auto text-center ">
+            <img className="w-20 h-20 mx-auto mb-4" src="https://vueschool.io/images/workshop/remote-vuejs-workshop.svg" alt="" />
+            <h2 className="text-3xl font-bold mb-4 "> Python Basics </h2>
+            <p className="text-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad id voluptatem, illo eum eius minus quia. Eos, quibusdam fugit, dicta natus voluptatum aperiam repudiandae inventore voluptas modi voluptatibus ex eveniet.</p>
+            </div>
         </div>
     )
 
--- jobboard/app/blog/page.tsx
+++ jobboard/app/blog/page.tsx
@@ -1,10 +1,43 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import Link from "next/link"
 
 
-function PostList(){
+function PostList() {
 
-    return(
+    return (
         <div>
-            <h3> Post List </h3>
+            <div>
+                <nav className="flex items-center text-white p-5">
+                    <span className="text-xl font-bold mx-auto"> Our Blog </span>
+
+                </nav>
+            </div>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-3">
+                    <Card>
+                        <CardHeader>
+                            <img src="https://vueschool.io/images/workshop/remote-vuejs-workshop.svg" alt="image " />
+                            <CardTitle className="mt-5">Python Basics</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <p>Clearn python basics in 10 minutes</p>
+                        </CardContent>
+                        <CardFooter>
+                            <Link href="/blog/rrrrr" className="underline "> Read More </Link>
+                        </CardFooter>
+                    </Card>
+
+
+                </div>
+
+            </div>
         </div>
     )
 
